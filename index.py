aebe0812b962c08a659e428961dc5c7b114804be
Kur-Achan <you@example.com>
2024-02-27 11:18:30 +0300

    index.py

---
+++ index.py
@@ -0,0 +1,15 @@
+A = input ("Enter number")
+B = input("Type another number")
+
+Addition=(int (A) + int (B))
+print("The Addition is:", Addition)
+
+Subtraction=(int (A) - int (B))
+print("The Subtraction:" , Subtraction)
+
+Multiplication = (int (A) * int (B))
+print("The Multiplication:" , Multiplication)
+
+Division = (int (A) / int (B))
+print("The Division:", Division)
+
